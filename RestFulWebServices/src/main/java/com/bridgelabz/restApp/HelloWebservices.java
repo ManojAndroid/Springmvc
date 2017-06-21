531a7cef9a32ed26fba2893b77310417f1671f32
bridgeit <manojkmrsingh71@gmail.com>
2017-06-21 11:00:17 +0530

    Springrestwebservicesbasic

---
+++ RestFulWebServices/src/main/java/com/bridgelabz/restApp/HelloWebservices.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.restApp;
+	import javax.ws.rs.GET;
+	import javax.ws.rs.Path;
+	import javax.ws.rs.PathParam;
+	import javax.ws.rs.core.Response;
+	  
+	@Path("/hello")
+	public class HelloWebservices {
+		
+	  
+	    public HelloWebservices() {
+			
+		}
+
+		@GET
+	    @Path("/{name}")
+	    public Response getMsg(@PathParam("name") String name) {
+	  
+	        String output = "Welcome   : " + name;
+	  
+	        return Response.status(200).entity(output).build();
+	  
+	    }
+	  
+	}
+
+
